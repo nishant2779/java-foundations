2826c3b91a9df53b7508a5dc3aea45e434c108c1
nishant2779 <nishantranjan2779@gmail.com>
2026-07-16 12:19:43 +0530

    Implement currency converter from USD to INR code
    Added a currency converter that converts USD to INR.

--- Day01/CurrencyConverter.java
+++ Day01/CurrencyConverter.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("plese enter amount of usd you wanna convert to inr ");
+        double usd = in.nextDouble();
+        double exchangerate = 95.05;
+        System.out.println(usd*exchangerate + " inr");
+    }
+}
