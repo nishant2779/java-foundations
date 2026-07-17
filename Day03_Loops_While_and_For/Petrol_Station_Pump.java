9e1ef111fa45fe7f7dcd6553bf38394399b0bf30
nishant2779 <nishantranjan2779@gmail.com>
2026-07-17 09:57:02 +0530

    Implement petrol station pump functionality prototype

---
+++ Day03_Loops_While_and_For/Petrol_Station_Pump.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+      
+        double costperlitre = 105.45;
+      
+        System.out.print("Enter the amount of which petrol u want : ");    
+        double amount = in.nextDouble();
+      
+        double fule = amount/costperlitre;
+      
+        for(double i=0 ; i<fule ; i++){
+            System.out.println("petrol filled : " + i + "  cost : " + i*costperlitre);
+        }
+         double cost = amount;
+         if(fule>=20){
+             cost=cost-(10/100*amount);
+         }
+         System.out.println("fule filled = " + fule + " litre");
+          System.out.println("total cost = " + amount);
+           System.out.println("discount : 10%");
+            System.out.println("net payable amount : " + cost );
+    }
+}
