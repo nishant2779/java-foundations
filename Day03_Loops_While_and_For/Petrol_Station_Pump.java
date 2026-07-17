8e106128a653f606080802f90fd4747413530909
nishant2779 <nishantranjan2779@gmail.com>
2026-07-17 10:09:39 +0530

    Fix fuel loop increment and discount calculation

--- Day03_Loops_While_and_For/Petrol_Station_Pump.java
+++ Day03_Loops_While_and_For/Petrol_Station_Pump.java
@@ -10,12 +10,12 @@ public class Main{
       
         double fule = amount/costperlitre;
       
-        for(double i=0 ; i<fule ; i++){
+        for(double i=0 ; i<fule ; i=i+0.1){
             System.out.println("petrol filled : " + i + "  cost : " + i*costperlitre);
         }
          double cost = amount;
          if(fule>=20){
-             cost=cost-(10/100*amount);
+             cost=cost-(0.1*amount);
          }
          System.out.println("fule filled = " + fule + " litre");
           System.out.println("total cost = " + amount);
